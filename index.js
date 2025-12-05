52a71abc442dcb84e72a8417b1810573cc81c582
Ali Raza <zakiraza987@gmail.com>
2025-12-06 03:00:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+//1.Alerts: Showing Messages to the User
+
+//alert("Hello world");
+
+//alert("Ali raza");
+
+//let num = 50
+//alert(num);
+
+//let Messages = "Hello this is my first alert message"
+//alert(Messages);
+
+//let firstName = "Ali"
+//let lastName = "Raza"
+//alert(firstName + " " + lastName);
+
+
+
+//2.Variables for Strings: Declaring and Using Text Data
+
+//let userName = "Ali Raza"
+//alert(userName);
+
+//let welcomeMessage = "Welcome to my website!"
+//alert(welcomeMessage);
+
+//let firstName = "Ali"
+//let lastName = "Raza"
+//alert(firstName + " " + lastName);
+
+//let favoritequote = "The best way to get started is to quit talking and begin doing."
+//alert(favoritequote);
+
+//let city = "Karachi"
+//alert("Welcom to " + city);
+
+
+
+//3.Variables for Numbers: Storing and Using Numerical Data
+
+//let age = 25;
+//alert("I am " + age + " years old.");
+
+//let num1 = 5;
+//let num2 = 10;
+//let sum = num1 + num2;
+//alert("sum: " + sum);
+
+//let num1 = 9;
+//let num2 = 7;
+//let result = num1 - num2;
+//alert ("subtraaction result: " + result);
+
+//let num1 = 7;
+//let num2 = 9;
+//let result = num1 * num2;
+//alert("multiplication result: " + result);
+
+//let message= "My age is "+ 25 + " years old";
+//alert(message);
+
+
+
+//4.Math Expressions: Performing Arithmetic Operations
+
+//let x = 10;
+//let y = 5;
+//let sum = x + y;
+//alert("Sum: " + sum);
+
+//let x = 50;
+//let y = 5;
+//alert("Subtractions Result: " + (x - y));
+
+//let x = 17;
+//let y = 5;
+//alert("Multiplication Result: " + (x * y));
+
+//let x = 20;
+//let y = 4;
+//alert("Division Result: " + (x / y));
+
+//let x = 10;
+//let y = 3;
+//let remainder = x % y;
+//alert("Remainder: " + remainder);
+
+
+
+//5.Prompt: Getting Input from the User
+
+//let userName = prompt("Ali Raza:");
+//alert("Hello, " + userName + "!");
+
+//let age = prompt("25:");
+//alert("You are " + age + " years old.");
+
+//Convert input to string Number
+ // let num1 = Number(prompt("Enter first number:"));
+  //let num2 = Number(prompt("Enter second number:"));
+  //let sum = num1 + num2;
+  //let product = num1 * num2;
+  //alert("Sum: " + sum + "\nProduct: " + product);
+
+  //let favoriteColor = prompt("What is your favorite color?");
+  //alert("wow i like it " + favoriteColor + " too");
+
+    //let userCity = prompt("Enter your city:");
+   // alert("You are from " + userCity + ".");
+   
+
+
+
+
+
+
